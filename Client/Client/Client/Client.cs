6e5f0d74ddd9990559ca51692c000a3db61164bf
Blankpanda <Blankpanda8@gmail.com>
2015-10-26 12:23:40 -0400

    seperated both projects into two different projects

---
+++ Client/Client/Client/Client.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Client.Client
+{
+    class Client
+    {
+        public Entry.ClientRequestInfo settings; /* settings for the user to pass in*/
+
+        /* constructor requires the user to enter in a strucutre with settings.*/
+        public Client(Entry.ClientRequestInfo userSettings)
+        {
+            settings = userSettings;
+        }
+     
+        /*template... */
+        public void Start()
+        {
+            byte[] buffer = new byte[1024]; // used as a general buffer.
+            
+
+            try
+            {
+                // get the server and its port and connect it to an endpoint
+                IPAddress ip = IPAddress.Parse(settings.ip_address);
+                IPEndPoint remoteEP = new IPEndPoint(ip, settings.port_number);
+
+                try
+                {
+                    // socket used to send information
+                    Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                    sender.Connect(remoteEP);
+                  
+                    // intially we want to send a message to the server telling what IP is connecting to it
+                    string hostIpAddress = Net.GetHostIpAddress();
+
+                    byte[] msg = Encoding.ASCII.GetBytes(hostIpAddress);
+                    int sent = sender.Send(msg);
+                    int bytesRec = sender.Receive(buffer);
+
+                    Console.WriteLine("echo = " + Encoding.ASCII.GetString(buffer, 0, bytesRec));
+
+                    buffer = new byte[1024];
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+
+
+
+                }
+                catch (Exception)
+                {                    
+                    throw;
+                }
+
+            }
+            catch (Exception)
+            {                
+                throw;
+            }
+
+        }
+    }
+}
---
+++ Server/Chat Client/Server/ServerInit.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Chat_Client.Server
+{
+	class ServerInit 
+	{
+		
+	
+
+		/* settings of the server */
+		public struct ServerSettings
+		{
+			public int port_number, backlog;
+			public string server_ip_address, server_password ,server_name;
+
+			public ServerSettings(int port, int log, string ip, string name, string password)
+			{
+				port_number = port;
+				backlog = log;
+				server_ip_address = ip;
+				server_name = name;
+				server_password = password;
+			}
+			
+		}
+
+		
+
+		/* used to a new server entry */
+		public void Create()
+		{
+			ServerSettings settings = new ServerSettings();
+			ServerList serverlist = new ServerList();
+
+			// data validation
+			string  inStr = ""; 
+			int     inInt = 0;
+			
+
+			// settings.server_name
+			Console.WriteLine("Enter the name of the new server: ");
+			while (true)
+			{
+				inStr = Console.ReadLine();
+
+				if (!(string.IsNullOrEmpty(inStr)))
+				{
+					if (!(Directory.Exists(serverlist.MainServerDirectory + @"\" + inStr)))
+					{
+						settings.server_name = inStr;
+						break;
+					}
+					else
+					{
+						Console.WriteLine("The server already exists");
+					}
+				   
+				}
+				else
+				{
+					Console.WriteLine("please enter in a name for the server");
+				}                                  
+				inStr = "";
+			}
+
+
+			while (true)
+			{
+				Console.WriteLine("Enter in the password for this sever: ");
+				inStr = Console.ReadLine();
+
+				if (!(string.IsNullOrEmpty(inStr)))
+				{                   
+					settings.server_password = inStr;
+					break;
+				}
+				else
+				{
+					settings.server_password = "";
+				}
+
+			}
+			
+
+			// settings.backlog       
+			while(true)
+			{
+				Console.WriteLine("Enter the backlog for {0}: ", settings.server_name);
+				inInt = 0;
+				try
+				{
+					inInt = int.Parse( Console.ReadLine());
+					
+					settings.backlog = inInt;
+					break;
+				}
+				catch (Exception)
+				{
+					Console.WriteLine("Invalid Input, please enter in a number \n(hint): try 5");                    
+				}
+				
+
+			}         
+
+			// settings.port_number
+			
+			while (true)
+			{
+				Console.WriteLine("Enter the port number for {0}.\nif nothing is supplied the port will default to 7777.", settings.server_name);
+				inInt = 0;
+				try
+				{
+					string inp = Console.ReadLine(); // so we can read the input as a string first instead of directly parsing Console.ReadLine().
+					if (inp == "")
+					{
+						settings.port_number = 7777;
+						break;
+					}
+					else
+					{
+						inInt = int.Parse(inp);
+						settings.port_number = inInt;
+						break;
+
+					}
+					
+				}
+				catch (Exception)
+				{
+					Console.WriteLine("Invalid Input, please enter in a valid port number \n(hint: try 12000");
+				}
+				
+			}
+
+			
+
+			// settings_ip_address
+			IPHostEntry host = Dns.Resolve(Dns.GetHostName() ); // only need the IP as a string here
+
+			// TODO: iterate through the hosts address list to find the private address
+		   
+			settings.server_ip_address = host.AddressList[0].ToString();
+
+			Console.WriteLine(settings.server_name + " server created.");
+			CreateNewServer(settings);
+		}
+
+		
+
+	
+		private static void CreateNewServer(ServerSettings settings)
+		{
+			ServerList Servers = new ServerList();
+			Servers.Add(settings);
+		}
+		
+		/* this method is used when the server starts to search and load settings to be passed to the Listen() method */
+		public static ServerSettings Init(string ServerName)
+		{
+			ServerList SL = new ServerList();
+			string[] ServerList = SL.GetServerList(); // list of all directory paths
+			string ServerPath = "";
+			// ex: ServerList[0] = Server\Caleb
+			
+			
+			
+
+			// we want to remove the parent directory from the path so we can retrieve the name
+			// for checking
+			// then we populate ConfigurationFile with elements of the txt file.
+			// we then use a SeverSettings to pass into Listen()
+		   
+			for (int i = 0; i < ServerList.Length; i++)            
+				ServerList[i] = ServerList[i].Remove(0, SL.MainServerDirectory.Length + 1);
+
+
+			for (int i = 0; i < ServerList.Length; i++)
+			{
+				if ( ServerName == ServerList[i])
+				{
+					ServerPath = SL.MainServerDirectory + @"\" + ServerName;
+				}
+			}
+
+			//gets the configuration file from the Servers\myserver directory
+			DirectoryInfo ConfigFileInfo = new DirectoryInfo(ServerPath);
+			FileInfo[] files = ConfigFileInfo.GetFiles();
+			string ConfigFilePath = "";
+			
+			for (int i = 0; i < files.Length; i++)
+			{
+				if (files[i].Extension == ".txt" && files[i].Name.Contains("_Config"))
+				{
+					ConfigFilePath = files[i].FullName;
+				}
+   
+			}
+
+			// used when reading config file
+			Logger FileReader = new Logger(LogType.Type.READER);
+			List<string> ConfigurationFile = FileReader.ReadTextFileList(ConfigFilePath);
+			ServerSettings settings = new ServerSettings();
+
+			//TODO: could I just iterate through this and add through the structure to make it less hacky?
+			settings.server_name        = ConfigurationFile[1];
+			settings.server_password    = ConfigurationFile[2];
+			settings.backlog            =  int.Parse( ConfigurationFile[3] );
+			settings.server_ip_address  = ConfigurationFile[4];
+			settings.port_number        =  int.Parse ( ConfigurationFile[5] );
+		
+			return settings;
+		}       
+	}
+}
