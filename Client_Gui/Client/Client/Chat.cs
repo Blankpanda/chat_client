cb600f36db86283158d25be7f7f9de6709cca76e
Blankpanda <Blankpanda8@gmail.com>
2016-01-26 13:48:18 -0500

    ported classes from non gui client

---
+++ Client_Gui/Client/Client/Chat.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+   
+    class Chat
+    {
+        public const string EOF_FLAG = "<EOF>";
+        
+        /// <summary>
+        /// Constructs a message prompt and accepts input from a user.
+        /// </summary>
+        /// <returns></returns>
+        public string GetMessageFromStream()
+        {
+            Console.Write(":");
+            string input = Console.ReadLine();
+            return input;
+        }
+
+        public int SendMessage(System.Net.Sockets.Socket sender)
+        {
+            Enum MessageType = Message.MessageType.Message; // the type of message where sending here is a text message.
+            Message msg = new Message();
+
+            string ChatMessage = "";
+            ChatMessage = GetMessageFromStream();
+
+            byte[] message = Encoding.ASCII.GetBytes(ChatMessage + "<EOF>");
+            int sent = sender.Send(message);
+
+            return sent;
+        }
+
+        public int SendIP(System.Net.Sockets.Socket sender)
+        {
+
+            Message msg = new Message();
+
+            string HostIpAddress = 
+                Net.GetHostIpAddress();
+
+            // the type of message where sending here is a Ip address.
+            string type = 
+                "type:" + msg.GetMessageTypeByName(Message.MessageType.SentIP); 
+            
+            byte[] message = Encoding.ASCII.GetBytes(HostIpAddress + 
+                                                " connected. " +
+                                                 type + 
+                                                  Chat.EOF_FLAG);
+
+            int sent = sender.Send(message);
+
+            return sent;
+        }
+    }
+}
---
+++ Client_Gui/Client/Client/Message.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+	class Message
+	{
+		
+		public enum MessageType
+		{
+			SentIP = 1,
+			Message,
+
+		}
+	
+		public string GetMessageTypeByName(MessageType message)
+		{
+			string Type = Enum.GetName(typeof(MessageType), message);
+			return Type;
+		}
+	
+
+	}
+	
+}
